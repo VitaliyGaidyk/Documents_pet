f334a57bd9a14ea403689d81538d8ba091da718f
Vitaliy <Ishimura20@gmail.com>
2020-09-02 22:03:39 +0300

    sliderExample

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,15 @@ $(function () {
         slidesToShow: 4,
         slidesToScroll: 2,
         dots: true,
-        arrows: false
+        arrows: false,
+        focusOnSelect: true
+    });
+    $('.section__example-photo').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="../images/svg/arrow-slider-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="../images/svg/arrow-slider-right.svg" alt="">'
+
     });
 });
