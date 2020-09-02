801fdc49bf6a029a4e9fc0b9054b3be1083973ff
Vitaliy <Ishimura20@gmail.com>
2020-09-02 21:20:29 +0300

    dots

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function () {
-
+    $('.section__slider-program').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        dots: true,
+        arrows: false
+    });
 });
