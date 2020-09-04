4e475015770cc8ca4a4102f36afa08917ff5d11e
Vitaliy <Ishimura20@gmail.com>
2020-09-04 16:43:41 +0300

    fix adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -5,16 +5,72 @@ $(function () {
         slidesToScroll: 2,
         dots: true,
         arrows: false,
-        focusOnSelect: true
+        focusOnSelect: true,
+        responsive: [{
+                breakpoint: 1210,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 426,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: false,
+                }
+            }
+        ]
     });
     $('.section__example-photo').slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="../images/svg/arrow-slider-left.svg" alt="">',
-        nextArrow: '<img class="slider-arrows slider-arrows__right" src="../images/svg/arrow-slider-right.svg" alt="">'
-
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="../images/svg/arrow-slider-right.svg" alt="">',
+        responsive: [{
+                breakpoint: 1210,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 426,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: false,
+                }
+            }
+        ]
     });
+
     $('.icon').click(function () {
         $(this).toggleClass('active');
     })
